1cd41cd04e3cef9f1ab8d9aaa19b4911974b5c5e
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2025-04-05 21:26:41 +0300

    Websocket connection should resume/suspend instance

--- src/main/java/org/acerbulescu/handler/WebsocketHandler.java
+++ src/main/java/org/acerbulescu/handler/WebsocketHandler.java
@@ -1,23 +1,32 @@
 package org.acerbulescu.handler;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.acerbulescu.docker.DockerClient;
+import org.acerbulescu.instancemanager.InstanceManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Log4j2
 @Component
 @AllArgsConstructor
 public class WebsocketHandler extends TextWebSocketHandler {
   @Autowired
   private final DockerClient dockerClient;
 
+  @Autowired
+  InstanceManager instanceManager;
+
   @Override
   public void afterConnectionEstablished(WebSocketSession session) throws Exception {
     String instanceName = getInstanceName(session);
 
+    log.info("Incrementing static connections for instance={}", instanceName);
+    instanceManager.incrementStaticConnections(instanceName);
     dockerClient.attachToContainerIo(instanceName, session);
   }
 
@@ -26,6 +35,14 @@ public class WebsocketHandler extends TextWebSocketHandler {
     String instanceName = getInstanceName(session);
 
     dockerClient.writeToContainerIo(instanceName, message.getPayload());
+  }
+
+  @Override
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    String instanceName = getInstanceName(session);
+
+    log.info("Decrementing static connections for instance={}", instanceName);
+    instanceManager.decrementStaticConnections(instanceName);
   }
 
   private String getInstanceName(WebSocketSession session) {
